9250d9b7603350e8a77b647b4ecbcf19c5b0dee5
Alfred Law <alfred.lawsn@gmail.com>
2018-10-15 20:17:54 +0000

    implemented validations and testing for the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,29 @@ class User < ActiveRecord::Base
 
   has_many :reviews
 
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validate :pw_confirmation_matches
+  validates :password, length: { minimum: 5 }, allow_blank: false
+
+  validates :name, presence: true
+
+  validates :email, uniqueness: { case_sensitive: false }
+  # validates :email, format: { with: /\A[a-z0-9]+\z/ }
+
+  def pw_confirmation_matches
+    if password != password_confirmation
+      errors.add(:password_confirmation, "must match password")
+    end
+  end
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.where("lower(email) = ?", email.strip.downcase).first
+    if user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,112 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Validations' do
+
+    it "should not create new user successfully with no pw confirmation" do
+      user = User.new
+      user.name = "Alfred Law"
+      user.email = "alfred@lighthouse.com"
+      user.password = "abc123"
+      user.password_confirmation = nil
+      expect(user).not_to be_valid
+    end
+
+    it "should not create new user successfully with no pw " do
+      user = User.new
+      user.name = "Alfred Law"
+      user.email = "alfred@lighthouse.com"
+      user.password = nil
+      user.password_confirmation = "abc123"
+      expect(user).not_to be_valid
+    end
+
+    it "should not create new user successfully with different pw confirmation" do
+      user = User.new
+      user.name = "Alfred Law"
+      user.email = "alfred@lighthouse.com"
+      user.password = "cba123"
+      user.password_confirmation = "abc123"
+      expect(user).not_to be_valid
+    end
+
+    it "should not create new user successfully with 3 letter password" do
+      user = User.new
+      user.name = "Alfred Law"
+      user.email = "alfred@lighthouse.com"
+      user.password = "abc"
+      user.password_confirmation = "abc"
+      expect(user).not_to be_valid
+    end
+
+    it "should not create new user successfully without name" do
+      user = User.new
+      user.name = nil
+      user.email = "alfred@lighthouse.com"
+      user.password = "abc123"
+      user.password_confirmation = "abc123"
+      expect(user).not_to be_valid
+    end
+
+    it "should not create new user successfully if email not unique" do
+      user = User.new
+      user.name = "Alfred Law"
+      user.email = "alfred@lighthouse.com"
+      user.password = "abc123"
+      user.password_confirmation = "abc123"
+
+      user2 = User.new
+      user.name = "Alfred Law"
+      user.email = "alfred@lighthouse.com"
+      user.password = "abc123"
+      user.password_confirmation = "abc123"
+      expect(user2).not_to be_valid
+    end
+
+  end
+
+  describe '.authenticate_with_credentials' do
+
+    it "should successfully log in" do
+      user = User.new
+      user.name = "Alfred Law"
+      user.email = "alfred@lighthouse.com"
+      user.password = "abc123"
+      user.password_confirmation = "abc123"
+      user.save
+
+      user2 = User.authenticate_with_credentials("alfred@lighthouse.com", "abc123")
+
+      expect(user.id).to be(user2.id)
+    end
+
+    it "should successfully log in with varying capital letters" do
+      user = User.new
+      user.name = "Alfred Law"
+      user.email = "alfRED@lighthouse.com"
+      user.password = "abc123"
+      user.password_confirmation = "abc123"
+      user.save
+
+      user2 = User.authenticate_with_credentials("ALFRED@lightHOUSE.com", "abc123")
+      user2.save
+      expect(user.id).to be(user2.id)
+    end
+
+    it "should successfully log in with spaces in front and back of email" do
+      user = User.new
+      user.name = "Alfred Law"
+      user.email = "alfred@lighthouse.com"
+      user.password = "abc123"
+      user.password_confirmation = "abc123"
+      user.save
+
+      user2 = User.authenticate_with_credentials("     alfred@lighthouse.com   ", "abc123")
+      user2.save
+      expect(user.id).to be(user2.id)
+    end
+
+  end
+
 end
